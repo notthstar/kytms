af5ac4f6d2309bc75c07bb3689e2ad1e0794614e
Forest <forest1xiao@icloud.com>
2019-11-01 12:05:07 +0800

    运货单SQL语句改完

--- logistics_mgt_sys/src/main/java/com/t28/forest/operate/dao/DispatchOrderDao.java
+++ logistics_mgt_sys/src/main/java/com/t28/forest/operate/dao/DispatchOrderDao.java
@@ -1,10 +1,16 @@
 package com.t28.forest.operate.dao;
 
+import org.springframework.stereotype.Repository;
+
 /**
  * @author XiangYuFeng
  * @description 派车单DAO层接口
  * @create 2019/10/31
  * @since 1.0.0
  */
+@Repository
 public interface DispatchOrderDao {
+
+
+
 }
---
+++ logistics_mgt_sys/src/main/java/com/t28/forest/operate/vo/DispatchVO.java
@@ -0,0 +1,10 @@
+package com.t28.forest.operate.vo;
+
+/**
+ * @author XiangYuFeng
+ * @description 派车单页面展示对象
+ * @create 2019/11/1
+ * @since 1.0.0
+ */
+public class DispatchVO {
+}
