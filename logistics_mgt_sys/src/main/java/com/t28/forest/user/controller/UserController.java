de23513de3015c04b84c0f65f8d507d08236accb
Forest <forest1xiao@icloud.com>
2019-11-11 16:23:21 +0800

    用户模块已完成

--- logistics_mgt_sys/src/main/java/com/t28/forest/user/controller/UserController.java
+++ logistics_mgt_sys/src/main/java/com/t28/forest/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.t28.forest.user.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.t28.forest.core.entity.SysUser;
 import com.t28.forest.core.model.ReturnInfoModel;
 import com.t28.forest.core.utils.SimpleUtils;
@@ -11,7 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
 import java.util.Objects;
 
 /**
@@ -33,11 +33,33 @@ public class UserController {
         SysUser user = userService.login(sysUser.getCode(), DigestUtils.md5Hex(sysUser.getPassword()));
 
         if (Objects.isNull(user) || Objects.isNull(user.getId())) {
-            HashMap<String, Object> map = new HashMap<String, Object>(2);
-            map.put("msg", "用户名或密码错误");
-            return SimpleUtils.objectToJSON(map);
+            return SimpleUtils.objectToJSON(new ReturnInfoModel("用户名或密码错误！", false));
         }
         return SimpleUtils.objectToJSON(new ReturnInfoModel(user));
+    }
+
+    @RequestMapping("/register")
+    @ResponseBody
+    public String userRegister(SysUser sysUser) {
+        // 设置用户表主键
+        sysUser.setId(SimpleUtils.generateId());
+        // 设置用户密码为MD5加密
+        sysUser.setPassword(DigestUtils.md5Hex(sysUser.getPassword()));
+        // 设置用户创建时间为当前时间
+        sysUser.setCreateTime(new Date());
+        int result = userService.register(sysUser);
+        if (result > 0) {
+            return SimpleUtils.objectToJSON(new ReturnInfoModel(sysUser, "用户注册成功！", true));
+        }
+        return SimpleUtils.objectToJSON(new ReturnInfoModel(sysUser, "用户注册失败！", false));
+    }
+
+    @RequestMapping("/out")
+    @ResponseBody
+    public String userOut(HttpSession session) {
+        // 清除session中的数据
+        session.invalidate();
+        return SimpleUtils.objectToJSON(new ReturnInfoModel("用户退出完成！", true));
     }
 
 }
